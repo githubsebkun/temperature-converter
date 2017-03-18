cba10ced96477764b67611363bf7d4a56bb98ab7
githubsebkun <sebastiankunjappan@gmail.com>
2017-03-18 14:47:13 +0000

    made classes in src/steps

---
+++ src/main/java/com/packetpub/mastering/selenium/WrongPageException.java
@@ -0,0 +1,9 @@
+package com.packetpub.mastering.selenium;
+
+/**
+ * Created by sindh on 18/03/2017.
+ */
+public class WrongPageException extends RuntimeException{
+    public WrongPageException(String message){
+        super(message);
+    }}
---
+++ src/test/java/com/packetpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -0,0 +1,46 @@
+package com.packetpub.mastering.selenium.steps;
+
+import com.packetpub.mastering.selenium.Google;
+import com.packetpub.mastering.selenium.TemperatureConverterPage;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by sindh on 18/03/2017.
+ */
+public class ConversionStepDefs {
+    private WebDriver driver;
+    public Google googlePage;
+    public TemperatureConverterPage temperatureConverterPage;
+
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
+    public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
+        //driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\sindh\\Desktop\\Training\\AllDriversNew\\chromedriver.exe" );
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        googlePage = new Google(driver);
+        temperatureConverterPage = googlePage.goToTemperatureConversionPage();
+    }
+
+    @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
+    public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
+        //input Fahrenheit value
+        temperatureConverterPage.inputFahrenheit(98.6);
+    }
+
+    @Then("^It should be converted to Celsius as (\\d+) degree$")
+    public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
+        //assertion
+        assertEquals(temperatureConverterPage.actualResult(), temperatureConverterPage.expectedResult());
+        driver.quit();
+    }
+}
